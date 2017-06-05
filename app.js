9252215c66b4feefc2565995e5ce6e4232d5e9a8
jaxx2104 <jaxx2104@gmail.com>
2017-06-05 22:19:13 +0900

    Add placeholder

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ app.get('/', (req, res) => {
     res.send('hello world')
 })
 
+app.get('/users/:name?', (req, res) => {
+    if (!req.params.name) {
+        res.send(`hello, nobody!`)
+    } else {
+        res.send(`hello, ${req.params.name}!`)
+    }
+})
+
 app.listen(3000)
 console.log('server starting...')
